cf129bd5e9b40cee842a21e35dcad34e6601f882
Yusuf Emin Navruz <99680293+comeonalbatross@users.noreply.github.com>
2022-12-20 19:46:24 +0300

    Sayı tahmin etme oyunu için update.
    Bu şekilde kullanıcı devam edip etmediğini belirleyecek ve time kütüphanesi hatası almayacak.

--- Sayi Tahmin Etme/main.c
+++ Sayi Tahmin Etme/main.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[])
 {
-	int rasgelesayi,tahmin,deneme=0;
+	int rasgelesayi,tahmin,deneme=0,devam=1;
 	srand(time(NULL));
 	rasgelesayi=rand()%20+1;
+	while(devam==1){
 	do
 	{
 		printf("Herhangi bir sayi tutunuz (1-20): ");
 		scanf("%d",&tahmin);
+		deneme++;
 		if(rasgelesayi>tahmin)
 		{
 			printf("Yukari \n");
-			deneme++;
 		}
 		else if(rasgelesayi<tahmin)
 		{
 			printf("Asagi \n");
-			deneme++;
 		}
-		else
-		{
-			printf("Tebrikler Bildiniz! \n");
-			deneme++;
-		}
-	}
-	while(rasgelesayi!=tahmin);
+		
+	}while(rasgelesayi!=tahmin);
+	printf("Tebrikler, sayiyi bildiniz\n");
 	printf("Deneme Sayiniz: %d",deneme);
+	rasgelesayi=rand()%20+1;
+	deneme=0;
+	printf("Devam etmek icin 1, durmak icin 0 yaziniz.\n");
+	scanf("%d",&devam);
+	}
 	return 0;
 }
